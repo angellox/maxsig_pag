951c1e270f5731a6fcee88695e45e615b480b9f0
Ángel Cruz <angelcruz160798@gmail.com>
2021-10-28 15:13:20 -0500

    new features

--- js/app.js
+++ js/app.js
@@ -3,9 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function eventListener() {
-    posicionarMenu();
+    navegacionFija();
 }
 
-function posicionarMenu(){
+function navegacionFija(){
+    const nav = document.querySelector('.contenedor-nav');
+    const promo = document.querySelector('.info-promo');
 
+    window.addEventListener('scroll', function() {
+        if(promo.getBoundingClientRect().bottom < 0){
+            nav.classList.add('fijo');
+        } else {
+            nav.classList.remove('fijo');
+        }
+    });
 }
